ade8e0f069a09ba90b4978570dd7d5a68b2019c3
safenessinc-cellss <safeness.inc@gmail.com>
2026-06-22 15:39:44 -0300

    Convert API to CommonJS module format

--- freemail-hub/api/index.ts
+++ freemail-hub/api/index.ts
@@ -1,3 +1,7 @@
-import app from "../server.js";
-
-export default app;
+// api/index.js - Para Node.js (CommonJS)
+module.exports = (req, res) => {
+  res.status(200).json({ 
+    message: 'API funcionando!',
+    timestamp: new Date().toISOString()
+  });
+};
