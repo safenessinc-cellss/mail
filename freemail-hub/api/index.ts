857f4622a72dd551a590d923e2b4420b2ac914fc
safenessinc-cellss <safeness.inc@gmail.com>
2026-06-23 08:51:17 -0300

    Delete freemail-hub directory

--- freemail-hub/api/index.ts
+++
@@ -1,7 +0,0 @@
-// api/index.js - Para Node.js (CommonJS)
-module.exports = (req, res) => {
-  res.status(200).json({ 
-    message: 'API funcionando!',
-    timestamp: new Date().toISOString()
-  });
-};
