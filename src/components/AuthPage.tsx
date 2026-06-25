89e6eadd29309f1d826b604dbe4c4acbf269a851
safenessinc-cellss <safeness.inc@gmail.com>
2026-06-25 08:57:11 -0300

    Replace ParticlesBackground with DnsParticles import

--- src/components/AuthPage.tsx
+++ src/components/AuthPage.tsx
@@ -18,7 +18,7 @@ import {
   updateProfile 
 } from 'firebase/auth';
 import { Mail, Shield, AlertTriangle, ArrowLeft, Loader2, Sparkles, Key, ChevronRight } from 'lucide-react';
-import ParticlesBackground from './ParticlesBackground';
+import DnsParticles from "./DnsParticles";
 
 interface AuthPageProps {
   onAuthSuccess: (user: any) => void;
